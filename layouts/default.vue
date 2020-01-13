57165f49366ff85741d88e4b0c1533b8ff365ec5
Abu Yusuf <crazyprogrammer109@gmail.com>
2020-01-13 13:49:40 +0600

    hms frontend with nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app 
+    id="inspire"
+    dark
+  >
+  <v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp"
+      color="primary" gray app fixed align-center justify-end row>
+      <div style="width: 150px" class="ml-0">
+      </div>
+      <p class="title hidden-sm-and-down mb-0 theme-text-light">HMS Project</p>
+       
+      <v-spacer></v-spacer>
+      <v-btn icon to="/login" color="primary">
+        Login
+      </v-btn>
+      <v-btn icon @click="logout" color="primary">
+        <v-icon>power_settings_new</v-icon> 
+      </v-btn>
+
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy;hms-2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  color: primary;
+}
+</style>
---
+++ pages/sign-up.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-content>
+    <v-container
+      class="fill-height"
+      fluid
+    >
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col
+          cols="12"
+          sm="8"
+          md="4"
+        >
+          <v-card class="elevation-1">
+            <v-toolbar
+              color="primary"
+              dark
+              flat
+            >
+              <v-toolbar-title>Sign-up form</v-toolbar-title>
+              <v-spacer />
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  label="Username"
+                  name="username"
+                  prepend-icon="emoji_people"
+                  type="text"
+                />
+
+                <v-text-field
+                  label="Name"
+                  name="name"
+                  prepend-icon="person"
+                  type="text"
+                />
+
+                <v-text-field
+                  label="Email"
+                  name="email"
+                  prepend-icon="email"
+                  type="text"
+                />
+
+                <v-text-field
+                  id="password"
+                  label="Password"
+                  name="password"
+                  prepend-icon="lock"
+                  type="password"
+                />
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="primary">Sign-up</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  props: {
+    // source: String
+  }
+}
+</script>
