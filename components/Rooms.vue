a3c6b1c51b7521fd8cfb1391c6aa597b3a6181b2
Abu Yusuf <crazyprogrammer109@gmail.com>
2020-01-22 16:37:24 +0600

    room list grid representation

--- components/Rooms.vue
+++ components/Rooms.vue
@@ -1,48 +1,43 @@
 <template>
-  <v-layout
-    row
-    justify-center
-    align-center>
-    <v-flex class="ml-2 mr-2"
-            md6 v-for="item in items" :key="item.id">
-      <v-card>
-        <div>
-          <v-img
-            class="white--text align-end"
-            height="200px"
-            :src="item.image"
-          >
-            <v-card-title>{{ item.name }}</v-card-title>
-          </v-img>
+  <v-card
+    class="d-flex flex-wrap mb-6"
+    flat
+    tile
+  > 
+    <v-card class="ma-2" v-for="item in items" :key="item.id">
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        :src="item.image"
+      >
+        <v-card-title>{{ item.name }}</v-card-title>
+      </v-img>
 
-          <v-card-subtitle class="pb-0">Room cost(per-night): {{ item.price }}</v-card-subtitle>
+      <v-card-subtitle class="pb-0">Room cost(per-night): {{ item.price }}</v-card-subtitle>
 
-          <v-card-text class="text--primary">
-            <div>Room Size: {{ item.size }}</div>
+      <v-card-text class="text--primary">
+        <div>Room Size: {{ item.size }}</div>
 
-            <div>{{ item.details }}</div>
-          </v-card-text>
+        <div>{{ item.details }}</div>
+      </v-card-text>
 
-          <v-card-actions>
-            <v-btn
-              color="orange"
-              text
-            >
-              Details
-            </v-btn>
+      <v-card-actions>
+        <v-btn
+          color="orange"
+          text
+        >
+          Details
+        </v-btn>
 
-            <v-btn
-              color="orange"
-              text
-            >
-              Share
-            </v-btn>
-          </v-card-actions>
-        </div>
-      </v-card>
-    </v-flex>
-  </v-layout>
-  
+        <v-btn
+          color="orange"
+          text
+        >
+          Share
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-card>
 </template>
 
 <script>
